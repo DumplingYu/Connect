2390c52ff85d152c9f57206980b15065653a2c96
GeorgeYu <dumpling_yu@163.com>
2016-08-17 19:47:24 +0800

    Update Client to Alpha 1.0

--- Connect-Server.py
+++ Connect-Server.py
@@ -2,7 +2,63 @@
 # Server Edition
 # Pre-Alpha 1.1
 
-import socket, select, sys
+import socket
+import sys
+import threading
+import tkinter as tk
+
+class listen(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+            connection, addr = server.accept()
+            connections.append(connection)
+            while True:
+                try:
+                    data = connection.recv(RECV_BUFFER).decode('utf-8')
+                    peername = connection.getpeername()
+                    if peername not in nickname.keys():
+                        if data: nickname[peername] = data
+                        else: nickname[peername] = 'Anonymous'
+                        output('[INFO] Client %s:%s (%s) connected'%tuple(list(addr)+[nickname[peername]]))
+                        broadcast(connection, '[SERVER] %s entered the room.'%nickname[peername])
+                        continue
+                    if data: broadcast(connection, '[%s] %s'%(nickname[peername], data))
+                except Exception as e:
+                    output('[ERROR] %s'%e)
+                    output('[INFO] Client %s:%s (%s) disconnected'%tuple(list(addr)+[nickname[peername]]))
+                    broadcast(connection, '[SERVER] %s left the room'%nickname[addr])
+                    connection.close()
+                    connections.remove(connection)
+
+def GUI():
+    global root, chatLog, entryBox
+    #Root
+    root = tk.Tk()
+    root.title('Connect (Server)')
+    root.geometry('500x650')
+    root.resizable(False, False)
+    #Background
+    background = tk.Frame(root, bd=0, bg='black')
+    background.lower(belowThis=None)
+    background.place(x=0,y=0,height=650,width=500)
+    #Chat Log
+    chatLog = tk.Text(root, bd=0, bg='black', fg='green', cursor='arrow', highlightbackground='black', state='disabled')
+    chatLog.tag_configure('tag-center', justify='center')
+    chatLog.tag_configure('tag-left', justify='left')
+    chatLog.tag_configure('tag-right', justify='right')
+    #Srollbar
+    scrollbar = tk.Scrollbar(root, command=chatLog.yview)
+    chatLog['yscrollcommand'] = scrollbar.set
+    #Entry box
+    entryBox = tk.Text(root, bd=1, bg='black', fg='green', highlightbackground='green')
+    #Send Button
+    sendButton = tk.Button(root, bd=0, bg='blue', text='Send')
+    #Placing Widgets
+    chatLog.place(x=10, y=10, height=580, width=455)
+    scrollbar.place(x=472, y=13, height=574)
+    entryBox.place(x=10, y=595, width=405, height=45)
+    sendButton.place(x=420, y=597, width=67, height=41)
 
 def broadcast(exception_sock, message, newline=True):
     '''Broadcast a message to every connected nodes except for the server and the one who sent the message'''
@@ -12,76 +68,37 @@ def broadcast(exception_sock, message, newline=True):
         if sock != server and sock != exception_sock:
             try: sock.send(message)
             except Exception: #May be socket disconnection or network problem
-                write('[INFO] Client %s:%s (%s) disconnected'%tuple(list(addr)+[nickname[sock.getpeername()]]))
-                broadcast(sock, '\n\r[SERVER] %s left the room'%nickname[addr])
+                output('[INFO] Client %s:%s (%s) disconnected'%tuple(list(addr)+[nickname[sock.getpeername()]]))
+                broadcast(sock, '[SERVER] %s left the room'%nickname[addr])
                 sock.close()
                 connections.remove(sock)
 
-def write(message, newline=True):
-    '''Outputs a message to the server terminal'''
+def output(message, pos='end', tag='tag-center'):
+    '''Outputs a message to the GUI'''
     message = str(message)
-    if newline and message[-1] != '\n': message += '\n'
-    sys.stdout.write(message)
-    sys.stdout.flush()
+    if message[-1] != '\n': message += '\n'
+    chatLog.configure(state='normal')
+    chatLog.insert(pos, message, tag)
+    chatLog.configure(state='disabled')
+    chatLog.yview('end')
 
-write('[CONNECT] Welcome to Connect!')
+GUI()
+output('Welcome to Connect!')
+output('Starting...')
 
-write('[CONNECT] Please input the host: ', False)
-host = sys.stdin.readline().strip()
-if not host: host = 'localhost'
-while True:
-    write('[CONNECT] Please input the port: ', False)
-    port = sys.stdin.readline().strip()
-    if not port: port = 2189
-    try:
-        port = int(port)
-        break
-    except ValueError:
-        write('[ERROR] Invalid Input!')
-RECV_BUFFER = 2048
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+RECV_BUFFER = 4096
 MAX_CONNECTIONS = 20
-connections = []
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(MAX_CONNECTIONS)
+output('\nChat server started on %s:%d\n'%(HOST, PORT))
+connections = [server]
 nickname = {}
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((host, port))
-server.listen(MAX_CONNECTIONS)
-connections.append(server)
-write('[INFO] Chat server started on port %d'%port)
+for i in range(MAX_CONNECTIONS):
+    listen_thread = listen()
+    listen_thread.start()
 
-while True:
-    try:
-        #Get the list sockets which are ready to be read through select
-        read_sockets,write_sockets,error_sockets = select.select(connections,[],[])
-
-        for sock in read_sockets:
-            if sock == server: #New connection through server socket
-                connection, addr = server.accept()
-                connections.append(connection)
-             
-            else: #Incoming message from a client
-                try:
-                    data = sock.recv(RECV_BUFFER).decode('utf-8')
-                    peername = sock.getpeername()
-                    if peername not in nickname.keys():
-                        if data: nickname[peername] = data[:-1]
-                        else: nickname[peername] = 'Anonymous'
-                        write('[INFO] Client %s:%s (%s) connected'%tuple(list(addr)+[nickname[peername]]))
-                        broadcast(connection, '\n\r[SERVER] %s entered the room\n'%nickname[peername])
-                        continue
-                    if data:
-                        broadcast(sock, '\n\r[%s] %s'%(nickname[peername], data))
-                except Exception as e:
-                    print(e)
-                    write('[INFO] Client %s:%s (%s) disconnected'%tuple(list(addr)+[nickname[peername]]))
-                    broadcast(sock, '\n\r[SERVER] %s left the room'%nickname[addr])
-                    sock.close()
-                    connections.remove(sock)
-    except KeyboardInterrupt:
-        write('\b\b[INFO] Server closing...')
-        broadcast(server, '\n[SERVER] Chat room closed by server')
-        break
-server.close()
-write('[INFO] Server closed')
-sys.exit()
+root.mainloop()
